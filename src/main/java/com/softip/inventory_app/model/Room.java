0c83d142e03cb6f9bb97f366d9f366df2bc3408b
Mario Kubala <mario.kubala@outlook.com>
2025-09-14 23:07:23 +0200

    preparing rooms

---
+++ src/main/java/com/softip/inventory_app/model/Room.java
@@ -0,0 +1,24 @@
+package com.softip.inventory_app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Room() {}
+    public Room(String name) { this.name = name; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
