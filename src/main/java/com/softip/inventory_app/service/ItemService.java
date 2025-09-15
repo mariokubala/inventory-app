5309556046fc38fe500b76a1eb1af598ac558911
Mario Kubala <mario.kubala@outlook.com>
2025-09-15 14:01:21 +0200

    added item services or repos

--- src/main/java/com/softip/inventory_app/service/ItemService.java
+++ src/main/java/com/softip/inventory_app/service/ItemService.java
@@ -1,4 +1,26 @@
 package com.softip.inventory_app.service;
 
+import com.softip.inventory_app.model.Item;
+import com.softip.inventory_app.model.State;
+import com.softip.inventory_app.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> getItemsByState(State state) {
+        return itemRepository.findByStateOrderByPriceDesc(state);
+    }
+
+    public List<Item> getRemovedItems() {
+        return itemRepository.findByOutDateNotNullOrderByPriceDesc();
+    }
 }
