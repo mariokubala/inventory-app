93cb33b80cce4566be68b3ec3f5dd7c038978ea8
Mario Kubala <mario.kubala@outlook.com>
2025-09-15 14:12:19 +0200

    added controller

--- src/main/java/com/softip/inventory_app/controller/ItemController.java
+++ src/main/java/com/softip/inventory_app/controller/ItemController.java
@@ -1,4 +1,27 @@
 package com.softip.inventory_app.controller;
 
+import com.softip.inventory_app.model.Item;
+import com.softip.inventory_app.model.State;
+import com.softip.inventory_app.service.ItemService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/state/{state}")
+    public List<Item> getByState(@PathVariable String state) {
+        if ("removed".equalsIgnoreCase(state)) {
+            return itemService.getRemovedItems();
+        }
+        return itemService.getItemsByState(State.valueOf(state.toUpperCase()));
+    }
 }
