b5ce48cd1ab61a2f4e147d1e0355d04230ef3c57
Mario Kubala <mario.kubala@outlook.com>
2025-09-23 15:00:11 +0200

    addjusted gitquick.sh

--- gitquick.sh
+++ gitquick.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # prepare file:	nano gitquick.sh
 # 		chmod a+x gitquick.sh
-# usage:	gitquick "Commit message"
-# or:		./gitquick "Commit message"
-# or in Win:	git add . && git commit -m "xxx" && git push
+# usage Cmder:	gitquick.sh "Commit message"
+# or git-bash:	./gitquick "Commit message"
+# or Win-cmd:	git add . && git commit -m "Commit message" && git push
 
 if [ -z "$1" ]; then
-  echo "Usage: $(basename "$0") \"commit message\""
+  echo "Usage: ./$(basename "$0") \"commit message\""
   exit 1
 fi
 
